7ce03a2745527a5452d3e49b1c1871a6bebe74f3
vladelets-vselennoy <108176070+vladelets-vselennoy@users.noreply.github.com>
2023-09-01 13:20:46 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28888,6 +28888,6 @@ $RefreshReg$(_c, "Portfolio");
 },{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/proj.jpg":"juvXu","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"juvXu":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "proj.8ca002ea.jpg" + "?" + Date.now();
 
-},{"./helpers/bundle-url":"lgJ39"}],"lW6qc":[function() {},{}]},["1xC6H","ShInH","8lqZg"], "8lqZg", "parcelRequire6158")
+},{"./helpers/bundle-url":"lgJ39"}],"lW6qc":[function() {},{}]},["1xC6H","ShInH","8lqZg"], "8lqZg", "parcelRequire4cb5")
 
 //# sourceMappingURL=index.975ef6c8.js.map
